4b34fa7430721a5dc1d83cc7a47e0caa31e03c1e
Tuan Le <leanhtuan030523@gmail.com>
2024-04-06 19:14:23 +0700

    Create Token, TokenData

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -8,3 +8,10 @@ class RegisterRequest(BaseModel):
     username: constr(min_length=6)
     email: str
     password: constr(min_length=6)
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    id: str | None = None
