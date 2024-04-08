4a14564afe5546b4cfea3093aff9957127e81aad
Tuan Le <leanhtuan030523@gmail.com>
2024-04-08 09:10:42 +0700

    Add set cookie to login and create logout endpoint

--- app/routers/user_route.py
+++ app/routers/user_route.py
@@ -1,18 +1,19 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Response
 from schemas.user_schema import LoginRequest, RegisterRequest
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from db.database import get_db
 from crud.user_crud import create_user, get_user
 from utils import verify_password, create_access_token
-import validators
+from schemas.user_schema import Token
+from typing import Annotated
+from models.user_model import User
+from dependencies import get_current_user
 
 router = APIRouter(prefix="/auth")
 
 @router.post("/register")
 async def register(request: RegisterRequest, db: Session = Depends(get_db)):
-    if not validators.email(request.email):
-        raise HTTPException(status_code=400, detail=f"Email is not valid.")
     
     existing_user = get_user(request.email, db)
     if existing_user:
@@ -26,7 +27,7 @@ async def register(request: RegisterRequest, db: Session = Depends(get_db)):
         }
 
 @router.post("/login")
-async def login(request: LoginRequest, db: Session = Depends(get_db)):
+async def login(request: LoginRequest, response: Response, db: Session = Depends(get_db)):
     user = get_user(request.email, db)
     if not user:
         raise HTTPException(status_code=400, detail="There is no account registered with this email address.")
@@ -36,6 +37,17 @@ async def login(request: LoginRequest, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Incorrect password")
     
     access_token_expire = timedelta(minutes=30)
-    access_token = create_access_token(data={"sub": user.email}, expired_delta=access_token_expire)
+    access_token = create_access_token(data={"sub": str(user.id)}, expired_delta=access_token_expire)
     
-    return {"access_token": access_token}
+    response.set_cookie(key="url_shortener", value=access_token)
+    
+    return Token(access_token=access_token, token_type="bearer")
+
+@router.post("logout")
+async def logout(response: Response):
+    response.delete_cookie(key="url_shortener")
+    return {"message": "User logged out successfully"}
+    
+@router.get("/user")
+async def user(current_user: Annotated[User, Depends(get_current_user)]):
+    return current_user
